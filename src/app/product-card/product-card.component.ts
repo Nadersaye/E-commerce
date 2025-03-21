76132fb1830b1d6d04037b2852dd774a77496a49
Nader <ns1568@fayoum.edu.eg>
2025-03-21 21:22:09 +0200

    Enhance product card component with star rating and navigation to details page

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,13 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input,input} from '@angular/core';
-import { Product } from '../product';
+import { Product } from '../models/product';
+import { Router } from '@angular/router';
+import { StarRatingComponent } from "../shared/star-rating/star-rating.component";
 
 @Component({
   selector: 'app-product-card',
-  imports: [CommonModule],
+  imports: [CommonModule, StarRatingComponent],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
 export class ProductCardComponent {
  productItem= input.required<Product>() ;
+ constructor(private router: Router) { }
+
+
+ viewItemDetails(id:number)
+  {
+    console.log("Viewing item details for id: ");
+    this.router.navigate(['/product-details', id]);
+  }
 }
