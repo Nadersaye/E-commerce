cd6d6186fc8707df2cd50bb793c36d7ab2dd9851
Nader <ns1568@fayoum.edu.eg>
2025-03-21 21:23:45 +0200

    Replace products wrapper with navigation bar and router outlet in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductsWrapperComponent } from "./products-wrapper/products-wrapper.component";
+import { NavigationBarComponent } from "./navigation-bar/navigation-bar.component";
+import { ProductsListComponent } from "./products-list/products-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductsWrapperComponent],
+  imports: [RouterOutlet, NavigationBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
