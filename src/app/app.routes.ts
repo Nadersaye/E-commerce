f667f15ad4846d0fcbbecdbb5d0e1b10e39f4191
Nader <ns1568@fayoum.edu.eg>
2025-03-21 21:24:56 +0200

    Add routing configuration for product details, login, registration, cart, and not found components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { ProductsWrapperComponent } from './products-wrapper/products-wrapper.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductsListComponent } from './products-list/products-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ProductsListComponent,
+  },
+  {
+    path: 'product-details/:id',
+     component: ProductDetailsComponent
+   },
+   {
+     path:'login',
+     component:LoginComponent
+   },
+   {
+    path: 'register',
+    component: RegisterComponent
+   },
+   {
+    path: 'cart',
+    component: CartComponent
+   },
+   {
+     path:'**',
+     component:NotFoundComponent
+   }
+
+];
